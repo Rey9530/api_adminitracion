e26ebb24075e7e1f70cd7166713cd0da2da9d0a6
Rey9530 <56655911+Rey9530@users.noreply.github.com>
2023-03-17 01:24:01 -0600

    Fix bugs

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config()
+require('dotenv').config();
 import expres from 'express';
 import cors from 'cors';  
 
