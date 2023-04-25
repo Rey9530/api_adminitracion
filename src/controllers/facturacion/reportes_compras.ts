2f2bfe3cb8fc71d38680d7a8b2d45af9acc9005b
Reynaldo Alexander Ruiz Rosales <thebestalpha2.3@gmail.com>
2023-04-24 21:37:59 -0600

    listar de compras

--- src/controllers/facturacion/reportes_compras.ts
+++ src/controllers/facturacion/reportes_compras.ts
@@ -5,18 +5,16 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const libroCompras = async (req = request, resp = response) => {
-  // let { ids = 0 } = req.params;
-  // let id_sucursal = Number(ids);
   var desde1: any = req.query.desde!.toString().split("-");
   var hasta1: any = req.query.hasta!.toString().split("-");
   var desde = new Date(desde1[0], desde1[1] - 1, desde1[2], 0, 0, 0);
-  var hasta = new Date(hasta1[0], hasta1[1] - 1, hasta1[2], 23, 59, 59); 
+  var hasta = new Date(hasta1[0], hasta1[1] - 1, hasta1[2], 23, 59, 59);
   var data = await prisma.compras.findMany({
     where: {
       fecha_creacion: {
         gte: desde,
         lte: hasta,
-      }, 
+      },
     },
     include: { Proveedor: true },
   });
@@ -24,6 +22,43 @@ export const libroCompras = async (req = request, resp = response) => {
   resp.json({
     status: true,
     msg: "Listado de registros",
-    data, 
+    data,
+  });
+};
+
+export const obtenerListadoCompras = async (
+  req = request,
+  resp = response
+) => {
+  var desde: any = req.query.desde!.toString();
+  var hasta: any = req.query.hasta!.toString();
+  let { ids = 0 } = req.params;
+  let id_sucursal = Number(ids);
+
+  // fecha.setDate(fecha.getDate() + dias);
+  desde = new Date(desde);
+  hasta = new Date(hasta);
+  hasta.setDate(hasta.getDate() + 1);
+
+  const data = await prisma.compras.findMany({
+    where: {
+      fecha_creacion: {
+        gte: desde,
+        lte: hasta,
+      },
+      id_sucursal,
+    },
+    include: { Proveedor: true },
+    orderBy: [
+      {
+        id_compras: "asc",
+      },
+    ],
+  });
+
+  return resp.json({
+    status: true,
+    msg: "Success",
+    data,
   });
 };
--- src/routes/facturacion/reportes_compras.ts
+++ src/routes/facturacion/reportes_compras.ts
@@ -3,7 +3,7 @@ const router = expres.Router();
 import { check } from "express-validator"; 
 import { validarCampos } from "../../middlewares/validar-campos";
 import { validarJWT } from "../../middlewares/validar-jwt"; 
-import { libroCompras } from "../../controllers/facturacion/reportes_compras";
+import { libroCompras, obtenerListadoCompras } from "../../controllers/facturacion/reportes_compras";
  
 router.get(
   "/libro_compras",
@@ -15,4 +15,15 @@ router.get(
   ],
   libroCompras
 ); 
+
+router.get(
+  "/obtener_listado_compras",
+  [
+    validarJWT,
+    check("desde", "El parametro desde es requerido y debe ser formato fecha YYYY-mm-dd").not().isEmpty().isDate(),
+    check("hasta", "El parametro hasta es requerido y debe ser formato fecha YYYY-mm-dd").not().isEmpty().isDate(),
+    validarCampos,
+  ],
+  obtenerListadoCompras
+);
 export default router;
