8d774cc4dfe75192aa9e4aec45a8ae5a101e7bdc
Reynaldo Alexander Ruiz Rosales <thebestalpha2.3@gmail.com>
2023-05-15 14:43:22 -0600

    Zona Horaria UTC

---
+++ prisma/migrations/20230515204304_zonahoraria/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Agenda" ALTER COLUMN "fecha_creacion" SET DATA TYPE TIMESTAMPTZ;
+
+-- AlterTable
+ALTER TABLE "Catalogo" ALTER COLUMN "fecha_creacion" SET DATA TYPE TIMESTAMPTZ;
+
+-- AlterTable
+ALTER TABLE "Usuarios" ALTER COLUMN "fecha_creacion" SET DATA TYPE TIMESTAMPTZ;
