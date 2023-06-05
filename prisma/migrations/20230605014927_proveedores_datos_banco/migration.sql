de2c32132f9323a131173eedf6e987b9dbc3faf7
Rey9530 <alex.rey.rosales.95@gmail.com>
2023-06-04 20:57:08 -0600

    Fix bugs bancos

--- prisma/migrations/20230605014927_proveedores_datos_banco/migration.sql
+++ prisma/migrations/20230605014927_proveedores_datos_banco/migration.sql
@@ -1,10 +1,10 @@
 -- AlterTable
-ALTER TABLE "Proveedores" ADD COLUMN     "id_banco" INTEGER ,
+ALTER TABLE "Proveedores" ADD COLUMN     "id_banco" INTEGER NOT NULL DEFAULT 0,
 ADD COLUMN     "no_cuenta" TEXT DEFAULT '',
 ADD COLUMN     "tipo_cuenta" TEXT DEFAULT '';
 
 -- CreateIndex
-CREATE INDEX "fk_proveedor_banco_id" ON "Proveedores"("id_banco");
+-- CREATE INDEX "fk_proveedor_banco_id" ON "Proveedores"("id_banco");
 
 -- AddForeignKey
-ALTER TABLE "Proveedores" ADD CONSTRAINT "fk_proveedor_banco" FOREIGN KEY ("id_banco") REFERENCES "Bancos"("id_banco") ON DELETE NO ACTION ON UPDATE NO ACTION;
+-- ALTER TABLE "Proveedores" ADD CONSTRAINT "fk_proveedor_banco" FOREIGN KEY ("id_banco") REFERENCES "Bancos"("id_banco") ON DELETE NO ACTION ON UPDATE NO ACTION;
