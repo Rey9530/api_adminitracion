c80e1f5dfa49d92f1c7134d5f539ba4f9abaa670
Reynaldo Ruiz <d9@tkiero.app>
2023-08-29 11:11:58 -0600

    Fix bug in formatNumber v2

--- src/helpers/format_number.ts
+++ src/helpers/format_number.ts
@@ -1,11 +1,10 @@
 
-export const formatNumber = (numb:any) => {
-    var str = numb.toString().split("."); 
-    str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    if(str.length==1){
-      str[1] = "00";
-    }else{
-      str[1] = Number.parseFloat(str[1]).toFixed(2); 
-    }
-    return "$"+str.join(".");
+export const formatNumber = (numb: any) => {
+  numb = Number.parseFloat(numb).toFixed(2);
+  var str = numb.toString().split(".");
+  str[0] = str[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  if (str.length == 1) {
+    str[1] = "00";
   }
+  return "$" + str.join(".");
+}
